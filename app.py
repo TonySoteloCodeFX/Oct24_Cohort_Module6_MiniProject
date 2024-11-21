631f24b5a741a4c78195bc77b2cd0e9390081d6b
TonySoteloCodeFX <tonysotelo.vfx@gmail.com>
2024-11-21 12:27:42 -0800

    Cutomer/Order Models Added

--- app.py
+++ app.py
@@ -9,3 +9,32 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://root:{my_passwo
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
+# Models ---------------------------------------------------------------------
+class Customer(db.Model):
+    __tablename__ = 'customers'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255))
+    phone = db.Column(db.String(15))
+    orders = db.relationship('Order', backref='customer')
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey{'customers.id'})
+
+class CustomerAccount(db.Model):
+    __tablename__ = 'customer_accounts'
+    id = db.Column(db.Integer, primary_key=True)
+
+
+# Schemas -------------------------------------------------------------------
+class CustomerSchema(ma.Schema):
+    id = fields.Integer(dump_only=True)
+    name = fields.String(required=True)
+    email = fields.String(required=True)
+    phone = fields.String(required=True)
+
+    class Meta:
+        fields = ('id', 'name', 'email', 'phone')
