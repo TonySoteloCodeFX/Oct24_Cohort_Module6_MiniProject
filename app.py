9e49a34db7330ee631c6e95380ccfe862b011f38
TonySoteloCodeFX <tonysotelo.vfx@gmail.com>
2024-11-21 15:53:08 -0800

    CustomerSchema Added

--- app.py
+++ app.py
@@ -22,19 +22,62 @@ class Order(db.Model):
     __tablename__ = 'orders'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date, nullable=False)
-    customer_id = db.Column(db.Integer, db.ForeignKey{'customers.id'})
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
 
 class CustomerAccount(db.Model):
     __tablename__ = 'customer_accounts'
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    customer = db.relationship('Customer', backref='customer_account', uselist=False)
 
+order_product_association = db.Table('order_product_association',
+                         db.Column('order_id', db.Integer, db.ForeignKey('orders.id'), primary_key = True),
+                         db.Column('product_id', db.Integer, db.ForeignKey('product_catalog.id'), primary_key = True))
+
+class ProductCatalog(db.Model):
+    __tablename__ = 'product_catalog'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    orders = db.relationship('Order', secondary=order_product_association, backref=db.backref('product_catalog'))
 
 # Schemas -------------------------------------------------------------------
 class CustomerSchema(ma.Schema):
     id = fields.Integer(dump_only=True)
     name = fields.String(required=True)
-    email = fields.String(required=True)
-    phone = fields.String(required=True)
+    email = fields.String
+    phone = fields.String
 
     class Meta:
         fields = ('id', 'name', 'email', 'phone')
+
+customer_schema = CustomerSchema()
+customers_schema = CustomerSchema(many=True)
+
+# Customer Routes -------------------------------------------------------------------
+@app.route('/customers', methods=['POST'])
+def add_customer():
+    try:
+        customer_data = customer_schema.load(request.json)
+    except ValidationError as Err:
+        return jsonify(Err.messages), 400
+    
+    new_customer = Customer(name=customer_data['name'], email=customer_data['email'], phone=customer_data['phone'])
+    db.session.add(new_customer)
+    db.session.commit()
+    return jsonify({'Message': 'New customer has been added successfully.'}), 201
+
+@app.route('/customers', methods=['GET'])
+def get_customers():
+    customers = Customer.query.all()
+    return jsonify(customers_schema.dump(customers)), 200
+
+# Run Program --------------------------------------------------------------
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug=True)
